eb876d79cbb13c3156894fd40751de06cda74954
Brian-Adego <79045844+Brian-Adego@users.noreply.github.com>
2021-04-21 15:13:52 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-//global selectors
+//selectors
 const formWrapper = document.querySelector(".search-wrapper");
 const formSearch = formWrapper.querySelector(".search");
 const foodContainer = document.querySelector(".foods");
 const input = formSearch.querySelector("input");
 const listContainer = document.querySelector(".food-recipe");
+const button = document.querySelector(".btn-class.hide");
+let bannerText = document.querySelector(".banner");
 
 //EventListeners
 formSearch.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
+//display foods once the DOM contents are loaded
 window.addEventListener("DOMContentLoaded", () => {
   displayFoods(foods);
 });
 
+//search for foods and display the results
 input.onkeyup = (e) => {
   const searchString = e.target.value.toLowerCase();
   let displayRecipe = foods.filter((recipeItems) => {
@@ -35,6 +39,7 @@ function displayFoods(foodItems) {
   foodContainer.innerHTML = displayFood;
 }
 
+//to populate the recipes
 foods.forEach((recipe) => {
   const list = document.createElement("li");
   list.classList.add("rec");
@@ -45,9 +50,23 @@ foods.forEach((recipe) => {
 });
 
 function displayAnswer(element) {
+  //to display only the recipe of the food selected
   const id = element.id;
   const recipe = listContainer.childNodes[id];
   foodContainer.classList.add("hide");
   listContainer.classList.remove("hide");
   recipe.classList.remove("hide");
+  button.classList.remove("hide");
+  formSearch.classList.add("hide");
+  bannerText.classList.add("hide");
+
+  //back button
+  button.addEventListener("click", () => {
+    listContainer.className = "food-recipe hide";
+    recipe.classList.add("hide");
+    foodContainer.classList.remove("hide");
+    button.classList.add("hide");
+    formSearch.classList.remove("hide");
+    bannerText.classList.remove("hide");
+  });
 }
